0724dba714510f982e6540733da22ada7f8982ac
Verichima <verichima@yahoo.com>
2023-03-16 19:42:24 -0700

    updated Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - main function that prints the statement below
  *
  * Return: Always 0 successfully
- * /
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

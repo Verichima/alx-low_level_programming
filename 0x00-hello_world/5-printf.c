ec9321503d3ebbeaf6a0c5b26de49a812010bbe0
Verichima <verichima@yahoo.com>
2023-03-16 19:53:04 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The start of the statement printed
+ *
+ * Return: 0 when achieved success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}

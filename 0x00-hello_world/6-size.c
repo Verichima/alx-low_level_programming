6f533a117674b49f6b7dafbe510f334e2c11e1f8
Verichima <verichima@yahoo.com>
2023-03-16 21:19:29 -0700

    Some corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	Printf("Size of an int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	Printf("Size of a float: %lu btye(s)", sizeof(float))
+	Printf("Size of a float: %lu btye(s)", sizeof(float));
 	return (0);
 }

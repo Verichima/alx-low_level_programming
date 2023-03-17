777e3ce9380a101f0c0491ef22aac13be4d81fe7
Verichima <verichima@yahoo.com>
2023-03-16 19:27:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function that prints the statement below
+ *
+ * Return - End the statement successfully
+ * /
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

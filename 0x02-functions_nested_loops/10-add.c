ec59344e1e84557141046adcb948121f06b3e961
Verichima <verichima@yahoo.com>
2023-03-21 16:06:20 -0700

    A plus B

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

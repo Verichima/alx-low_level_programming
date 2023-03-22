c1b0a77cdcd037bcf46e0fa19f9672203ad40eda
Verichima <verichima@yahoo.com>
2023-03-21 20:53:30 -0700

    timetables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,40 @@
 #include "main.h"
 /**
- * add - function that adds two integers and returns the result
+ * times_table - function that adds two integers and returns the result
  *
- * @a: first parameter
- * @b: second parameter
  * Return: always 0
  */
 
-int add(int a, int b)
+void times_table(void)
 {
-	return (a + b);
+		int x, y, z, u, d;
+
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = 0; y <= 9; y++)
+			{
+				z = x * y;
+				if (z > 9)
+				{
+					u = z % 10;
+					d = (z - u) / 10;
+					_putchar(44);
+					_putchar(32);
+					_putchar(d + '0');
+					_putchar(u + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(44);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+
 }

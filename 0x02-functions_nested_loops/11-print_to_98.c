a895ffcfcc9818e8225db46bf5060b8757ace11d
Verichima <verichima@yahoo.com>
2023-03-21 17:27:16 -0700

    Battery

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
-			if (i !=98)
+			if (i != 98)
 				printf(", ");
 		}
 		printf("\n");
@@ -29,8 +29,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
-			if (i !=98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -39,8 +39,8 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d", i);
-			if (i !=98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}

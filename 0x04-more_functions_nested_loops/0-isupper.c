9428ddca451dbfbdd5aad840c2b78732cfa2dd66
Verichima <verichima@yahoo.com>
2023-03-23 11:44:09 -0700

    Uppercase and Lowercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+ #include "main.h"
+
+/**
+ * _isupper - function to check if letter is upper case
+ * @c: the parameter to check
+ * Return: 1 for uppercase or 0 for lower case
+ */
+
+int _isupper(int c);
+
+{
+		if (c >= 65 && c <= 90)
+		{
+			return (1);
+		}
+		return (0);
+}

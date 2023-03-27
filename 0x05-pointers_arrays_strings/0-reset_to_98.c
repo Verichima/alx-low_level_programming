26d29f856b640fbce194d8552c997da58e00ba91
Verichima <verichima@yahoo.com>
2023-03-27 10:47:57 -0700

    Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int as parameter
+ * updates the value it points to 98
+ * @n: The input pointer of an integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A project on Pointers, Arrays and Strings

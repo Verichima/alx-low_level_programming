2e27bc6ba3a38aecfa5210968abbed49657335a2
Verichima <verichima@yahoo.com>
2023-04-03 19:37:16 -0700

    Life of a ragged diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Function that prints the sum of two diagonals
+ * @a: input value
+ * @size: input value
+ *
+ * Return: Nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, b;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 = sum1 + a[b * size + b];
+	}
+
+	for (b = size - 1; b >= 0; b--)
+	{
+		sum2 = sum2 + a[b * size + (size - b - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}

729e6c63dce4a7cb4ffc4099894ecd2192425843
Verichima <verichima@yahoo.com>
2023-04-05 19:15:13 -0700

    Impossible to eradicate updated

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns the value of x
  * @x: First input value
  * @y: Second input value
  *

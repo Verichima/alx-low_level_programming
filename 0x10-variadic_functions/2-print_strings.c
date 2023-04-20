91df60d838a333aaae55bb1ee4d1536248747862
Verichima <verichima@yahoo.com>
2023-04-20 10:29:23 -0700

    One woman's constant

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - function that prints strings
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
+ * @...: Another value to be passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list words;
+	char *alphabet;
+	unsigned int num;
+
+	va_start(words, n);
+	for (num = 0; num < n; num++)
+	{
+		alphabet = va_arg(words, char *);
+		if (alphabet == NULL)
+			printf("nil");
+		else
+			printf("%s", alphabet);
+
+		if (num != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(words);
+}

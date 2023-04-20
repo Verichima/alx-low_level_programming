d379e28f8681da7db053e8ef8582e5193459f323
Verichima <verichima@yahoo.com>
2023-04-20 11:24:20 -0700

    One's woman constant updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,6 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (num != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	printf("\n");
+
 	va_end(words);
+	printf("\n");
 }

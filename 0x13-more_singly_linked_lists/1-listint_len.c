382b11ff410188de85391c926c94f66731c9172b
verichima <verichima@yahoo.com>
2023-05-02 10:39:58 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - Function that returns the number of elements 
+ * listint_len - Function that returns the number of elements
  * @h: Value input of linked list that traverse
  *
  * Return: number of nodes

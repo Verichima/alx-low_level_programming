fbbae652b65135e3e00ceaac50e1f436df056411
verichima <verichima@yahoo.com>
2023-05-02 20:38:46 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Function that frees a listint_t
+ * @h: value input in the linked list
+ * Returns: the size of the list that was freed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t length = 0;
+	int different;
+	listint_t *temporal;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		different = *h - (*h)->next;
+		if (different > 0)
+		{
+			temporal = (*h)->next;
+			free(*h);
+			*h = temporal;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+
+	*h = NULL;
+	return (length);
+}
+
